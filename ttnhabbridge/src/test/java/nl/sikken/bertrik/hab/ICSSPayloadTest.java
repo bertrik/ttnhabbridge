d9cbf98f3e4ed09a5b6027fba2e029ca7856bf5d
MedadRufus <mailto@medadnewman.co.uk>
2020-11-24 22:36:08 +0000

    Put new tests for latest payload

--- ttnhabbridge/src/test/java/nl/sikken/bertrik/hab/ICSSPayloadTest.java
+++ ttnhabbridge/src/test/java/nl/sikken/bertrik/hab/ICSSPayloadTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
  */
 public final class ICSSPayloadTest {
 	
-    byte[] data = hexStringToByteArray("7e7a5167ca1712d3f200ca1712d3f20000c8ca1712d3f20000c800");
+    byte[] data = hexStringToByteArray("6303c845b71ee0ff3e01b71ee0ff440149e107b71ee0ff4c016ee0070000000000002e3407b71ee0ff280131e007b71ee0ff440149e107b71ee0ff4c016ee0070000000000002e3407b71ee0ff280131e007b71ee0ff440149e107b71ee0ff4c016ee0070000000000002e3407b71ee0ff280131e007");
     ICSSPayload payload = ICSSPayload.parse(data); 
     
     /**
@@ -24,48 +24,64 @@ public final class ICSSPayloadTest {
     
     @Test
     public void testParseVoltage() {
-        Assert.assertEquals(43, payload.getloadVoltage());
-        Assert.assertEquals(33, payload.getnoloadVoltage());
+        Assert.assertEquals(30, payload.getloadVoltage());
+        Assert.assertEquals(30, payload.getnoloadVoltage());
     }
     
     @Test
     public void testParsetemp() {
-        Assert.assertEquals(-23, payload.getBoardTemp());
+        Assert.assertEquals(13, payload.getBoardTemp());
     }
     
     @Test
     public void testParsepressure() {
-        Assert.assertEquals(400, payload.getPressure());
+        Assert.assertEquals(1000, payload.getPressure());
     }
     
     @Test
     public void test_getData_received_flag() {
-        Assert.assertEquals(1, payload.getData_received_flag());
+        Assert.assertEquals(0, payload.getData_received_flag());
     }
     
     @Test
     public void test_getNumSats() {
-        Assert.assertEquals(12, payload.getNumSats());
+        Assert.assertEquals(8, payload.getNumSats());
     }
     
     @Test
     public void test_getReset_cnt() {
-        Assert.assertEquals(7, payload.getReset_cnt());
+        Assert.assertEquals(5, payload.getReset_cnt());
     }
     
     @Test
     public void test_long_lat() {
-        Assert.assertEquals(-75.378357, payload.getLongitude(),0.00001);
-        Assert.assertEquals(39.910815, payload.getLatitude(),0.00001);
+        Assert.assertEquals(-0.20971199, payload.getLongitude(),0.00001);
+        Assert.assertEquals(51.53017044, payload.getLatitude(),0.00001);
 
     }
     
     @Test
     public void test_getAltitude() {
-        Assert.assertEquals(61, payload.getAltitude());
+        Assert.assertEquals(81, payload.getAltitude());
 
     }
     
+    
+    @Test
+    public void test_getPast_position_times_size() {
+    	Assert.assertEquals(12, payload.getPast_position_times().size());
+    }
+    
+    
+    @Test
+    public void test_getPast_position_times() {
+   	
+    	Assert.assertEquals(51.53017044, payload.getPast_position_times().get(0).getLatitude(),0.00001);
+    	Assert.assertEquals(-0.209711998, payload.getPast_position_times().get(0).getLongitude(),0.00001);
+    	Assert.assertEquals(82, payload.getPast_position_times().get(0).getAltitude());
+    	Assert.assertEquals(1608825961, payload.getPast_position_times().get(0).getUnix_time());
+    }
+ 
     
     public byte[] hexStringToByteArray(String s) {
         int len = s.length();

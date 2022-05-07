f54fb58494cbe9e463fc38b4c992e0dc2ca9c0e5
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-05-07 18:14:52 +0100

    Update IAmateurSondehubRestApi.java

--- ttnhabbridge/src/main/java/nl/sikken/bertrik/hab/amateurSondehub/IAmateurSondehubRestApi.java
+++ ttnhabbridge/src/main/java/nl/sikken/bertrik/hab/amateurSondehub/IAmateurSondehubRestApi.java
@@ -1,5 +1,6 @@
 package nl.sikken.bertrik.hab.amateurSondehub;
 
+import nl.sikken.bertrik.hab.UploadResult;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -14,9 +15,6 @@ public interface IAmateurSondehubRestApi {
 
     @PUT("/habitat/_design/payload_telemetry/_update/add_listener/{doc_id}")
     Call<String> updateListener(@Path("doc_id") String docId, @Body String json);
-
-    @GET("/_uuids")
-    Call<UuidsList> getUuids(@Query("count") int count);
     
     @PUT("/habitat/{doc_id}")
     Call<UploadResult> uploadDocument(@Path("doc_id") String docId, @Body String document);
